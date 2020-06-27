949b0808dcebbaa232e36271b977bac190c37411
Jake Lever <jake.lever@gmail.com>
2020-06-26 17:01:49 -0700

    Add script for downloading new CORD19 data

---
+++ data/downloadCORD19_metadataOnly.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -ex
+
+mkdir -p cord19
+
+rm -fr tmp_cord
+mkdir tmp_cord
+cd tmp_cord
+
+curl --silent https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases.html |\
+grep -oP "https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases/cord-19_.*?.tar.gz" |\
+sort |\
+tail -n 1 > cord19_listing.txt
+
+file_url_count=`cat cord19_listing.txt | wc -l`
+if [[ $file_url_count -ne 1 ]]; then
+	echo "ERROR: Couldn't find URL of latest CORD-19 file"
+	exit 1
+fi
+
+file_date=`grep -oP "\d\d\d\d-\d\d-\d\d" cord19_listing.txt`
+file_url=`cat cord19_listing.txt`
+
+wget $file_url
+
+tar -zxvf cord-19_$file_date.tar.gz $file_date/metadata.csv
+
+mv $file_date/metadata.csv ../cord19/metadata.csv
+echo $file_date > ../cord19/date.txt
+
+cd ..
+rm -fr tmp_cord
+
