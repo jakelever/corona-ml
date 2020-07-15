6827b6eb94c624f8ba989c99a688e85eae62b245
Jake Lever <jake.lever@gmail.com>
2020-07-15 16:03:11 -0700

    Refactoring and added SVD-based classification pipeline with high threshold

--- pipeline/doTopics.py
+++ pipeline/doTopics.py
@@ -7,6 +7,8 @@ from utils import DocumentVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser('Annotate the topics of the documents')
@@ -22,60 +24,76 @@ if __name__ == '__main__':
 	
 	annotated = [ d for d in documents if len(d['annotations']) > 0 ]
     
-	filters = {'RemoveFromCorpus?','NotAllEnglish','NotRelevant','Skip','Maybe'}
-	filters.update({'Review','Updates','Comment/Editorial','News','Meta-analysis'})
-	#filters.update({'Updates','Comment/Editorial','News','Meta-analysis','Guidelines'})
-	#filters.update({'News'})
+	toRemoveFromTraining = {'RemoveFromCorpus?','NotAllEnglish','NotRelevant','Skip','Maybe','FixAbstract'}
+	#toRemoveFromTraining.update({'Review','Updates','Comment/Editorial','News','Meta-analysis'})
+	#toRemoveFromTraining.update({'Updates','Comment/Editorial','News','Meta-analysis','Guidelines'})
+	#toRemoveFromTraining.update({'News'})
 
-	annotated = [ d for d in annotated if not any (f in d['annotations'] for f in filters) ]
+	annotated = [ d for d in annotated if not any (f in d['annotations'] for f in toRemoveFromTraining) ]
 
-	toRemove = ['SARS-CoV','MERS-CoV','SARS-CoV-2','None','NotMainFocus']
-	toRemove.append('Clinical Trial')
-	#toRemove.append('Review','Comment/Editorial','Meta-analysis','News','NotRelevant')
+	annotationsToStrip = ['SARS-CoV','MERS-CoV','SARS-CoV-2','None','NotMainFocus']
+	annotationsToStrip.append('Clinical Trial')
+	annotationsToStrip.extend(['Review','Comment/Editorial','Meta-analysis','News','NotRelevant'])
 	
 	groupings = {}
-	groupings['Drug Repurposing'] = 'Therapeutics'
-	groupings['Novel therapeutics'] = 'Therapeutics'
+	#groupings['Drug Repurposing'] = 'Therapeutics'
+	groupings['Novel therapeutics'] = 'Novel Therapeutics'
 	groupings['Host Biology'] = 'Molecular Biology'
 	groupings['Viral Biology'] = 'Molecular Biology'
-	groupings['Case Report / Series'] = 'Patient Reports'
-	groupings['Observational Study'] = 'Patient Reports'
-	groupings['Forecasting/Modelling'] = 'Disease Modelling'
+	groupings['Case Report / Series'] = 'Case Reports'
+	#groupings['Observational Study'] = 'Patient Reports'
+	groupings['Forecasting/Modelling'] = 'Forecasting & Modelling'
+	groupings['Healthcare Strategy / Preparedness'] = 'Preparedness'
+	groupings['Other medical disciplines'] = 'Medical Disciplines'
+	
 	
 	for g in groupings:
 		assert any( a == g for d in documents for a in d['annotations']), "Couldn't find any annotations for %s" % g
 	
 	for d in documents:
 		d['annotated_topics'] = d['annotations']
-		d['annotated_topics'] = [ a for a in d['annotated_topics'] if not a in toRemove ]
+		d['annotated_topics'] = [ a for a in d['annotated_topics'] if not a in annotationsToStrip ]
 		d['annotated_topics'] = [ (groupings[a] if a in groupings else a) for a in d['annotated_topics'] ]
 		d['annotated_topics'] = sorted(set(d['annotated_topics']))
 		
-	print("Vectorizing...")
-	documentVectorizer = DocumentVectorizer()
-	X = documentVectorizer.fit_transform(annotated)
-	X_all = documentVectorizer.transform(documents)
+	#print(Counter( at for d in annotated for at in d['annotated_topics'] ))
+	#assert False
+		
+	#print("Vectorizing...")
+	#documentVectorizer = DocumentVectorizer()
+	#X = documentVectorizer.fit_transform(annotated)
+	#X_all = documentVectorizer.transform(documents)
 	
 	
 	encoder = MultiLabelBinarizer()
 	y = encoder.fit_transform( [ d['annotated_topics'] for d in annotated ] )
 	
+	pipeline = Pipeline([
+		("vectorizer", DocumentVectorizer(features=['titleabstract'])),
+		("dimreducer", TruncatedSVD(n_components=64,random_state=0)),
+		("classifier", OneVsRestClassifier(LogisticRegression(class_weight='balanced',random_state=0,C=21)))
+	])
+	
 	#label_to_column = { l:i for i,l in enumerate(encoder.classes_) }
 	#print(encoder.classes_)
-	print(Counter( sorted([ a for d in annotated for a in d['annotated_topics'] ]) ))
+	#print(Counter( sorted([ a for d in annotated for a in d['annotated_topics'] ]) ))
 	
-	print("Training...")
-	clf = OneVsRestClassifier(LogisticRegression(class_weight='balanced',random_state=0))
-	clf.fit(X,y)
+	#print("Training...")
+	#clf = OneVsRestClassifier(LogisticRegression(class_weight='balanced',random_state=0))
+	pipeline.fit(annotated,y)
 	
-	predictions = clf.predict(X_all)
+	probs = pipeline.predict_proba(documents)
+	
+	print("Selecting high confidence predictions")
+	probs_mask = (probs > 0.75)	
+	probs = probs * probs_mask
 	
 	for d in documents:
 		if not 'topics' in d:
 			d['topics'] = []
 	
 	topicCounter = Counter()
-	for doc_index,label_index in zip(*predictions.nonzero()):
+	for doc_index,label_index in zip(*probs.nonzero()):
 		d = documents[doc_index]
 		#print(doc_index,label_index)
 			
@@ -85,10 +103,11 @@ if __name__ == '__main__':
 			topic = encoder.classes_[label_index]
 			d['topics'].append(topic)
 			topicCounter[topic] += 1
-			
+						
 	# Cleanup and note the documents that were excluded
 	for d in documents:
-		d['exclude'] = any (f in d['annotations'] for f in filters)
+		d['topics'] = sorted(set(d['topics']))
+		d['exclude'] = any (f in d['annotations'] for f in toRemoveFromTraining)
 		del d['annotated_topics']
 						
 	print(topicCounter)
