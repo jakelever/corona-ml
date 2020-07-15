8a95ce4f2d543fa5dd87fb937e261cd885e3b510
Jake Lever <jake.lever@gmail.com>
2020-07-15 16:01:30 -0700

    Start of reorganising NER code for multiple entity types

--- pipeline/doNER.py
+++ pipeline/doNER.py
@@ -3,6 +3,7 @@ import argparse
 import pickle
 import json
 from collections import defaultdict,Counter
+from scipy.spatial import distance_matrix
 
 def doesLocationCapitalizationMatch(allEntities,e):
 	if e['type'] != 'location':
@@ -14,15 +15,17 @@ def doesLocationCapitalizationMatch(allEntities,e):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser('Run named entity recognition on parsed documents and integrate it into the JSON data')
 	parser.add_argument('--inJSON',required=True,type=str,help='Filename of JSON documents')
+	parser.add_argument('--viruses',required=True,type=str,help='JSON file with virus entities')
 	parser.add_argument('--drugs',required=True,type=str,help='JSON file with drug entities')
 	parser.add_argument('--geonames',required=True,type=str,help='JSON file with geographic entities')
+	parser.add_argument('--custom',required=True,type=str,help='JSON file with custom additional entities')
 	parser.add_argument('--inParsed',required=True,type=str,help='Filename of Kindred corpus')
 	parser.add_argument('--outJSON',required=True,type=str,help='Output JSON with entities')
 	args = parser.parse_args()
 	
 	print("Loading wordlists...")
 		
-	nerFiles = {'drug':args.drugs,'location':args.geonames}
+	nerFiles = {'virus':args.viruses,'drug':args.drugs,'location':args.geonames,'custom':args.custom}
 	
 	allEntities = {}
 	termLookup = defaultdict(set)
@@ -34,43 +37,38 @@ if __name__ == '__main__':
 			
 			for entityID,entity in entityData.items():
 				for alias in entity['aliases']:
-					termLookup[alias.lower()].add((entityType,entityID))
-
-	virus_keywords = {}
-	virus_keywords[('SARS-CoV-2','Q82069695')] = ['covid','covid-19','covid 19','sars-cov-2','sars cov 2','sars-cov 2','sars cov-2','sars-cov2','sars cov2','sarscov2','2019-ncov','2019 ncov','ncov19','ncov-19','ncov 19','ncov2019','ncov-2019','ncov 2019','(sars)-cov-2','(sars) cov 2','(sars)-cov 2','(sars) cov-2','(sars)-cov2','(sars) cov2','(sars)cov2','severe acute respiratory syndrome coronavirus 2','2019 novel coronavirus','novel 2019 coronavirus']
-	virus_keywords[('SARS-CoV','Q85438966')] = ['sars','sars-cov','sars cov','sars-cov-1','sars cov 1','sars-cov 1','sars cov-1','sars-cov1','sars cov1','sarscov1','severe acute respiratory syndrome', '(sars)-cov','(sars) cov','(sars)-cov-1','(sars) cov 1','(sars)-cov 1','(sars) cov-1','(sars)-cov1','(sars) cov1','(sars)cov1', 'sars virus']
-	virus_keywords[('MERS-CoV','Q4902157')] = ['mers','middle east respiratory syndrome','mers-cov','mers cov','mers-cov','mers cov','mers cov','mers-cov','merscov','(mers)-cov','(mers) cov','(mers)-cov','(mers) cov','(mers) cov','(mers)-cov','(mers)cov','mers virus']
-
-	for (virus,entityID),keywords in virus_keywords.items():
-		for keyword in keywords:
-			termLookup[keyword.lower()].add(('virus',entityID))
-			allEntities[entityID] = {'id':entityID,'name':virus}
-			
-	termLookup['k-mers'].add(('conflicting','Q6322851'))
-	allEntities['Q6322851'] = {'id':'Q6322851','name':'k-mers'}
-
-	manualChemicalAdditions = {}
-	manualChemicalAdditions['Q200253'] = ['traditional chinese medicine','chinese herbal medicine','emodin']
-	manualChemicalAdditions['Q28209496'] = ['remdesivir']
-	manualChemicalAdditions['Q603502'] = ['convalescent plasma']
-	manualChemicalAdditions['Q798309'] = ['bcg vaccine','bacillus calmette guerin vaccine']
-
-	for chemicalID,chemicalNames in manualChemicalAdditions.items():
-		for chemicalName in chemicalNames:
-			termLookup[chemicalName.lower()].add(('chemical',chemicalID))
-			
-		mainTerm = chemicalNames[0]
-		allEntities[chemicalID] = {'id':entityID,'name':mainTerm}
+					tmpEntityType = entity['type'] if entityType == 'custom' else entityType
+					termLookup[alias.lower()].add((tmpEntityType,entityID))
 		 
 	termLookup = defaultdict(set,{ k:v for k,v in termLookup.items() if not '/' in k })
+	
+	with open(args.geonames,encoding='utf8') as f:
+		geoData = json.load(f)
+	
+	print("NO AMBIGUITY ALLOWED")
+	termLookup = defaultdict(set,{ k:v for k,v in termLookup.items() if len(v) == 1 })
+	
+	#for alias,entities in termLookup.items():
+	#	if len(entities) > 1:
+	#		print("%s\t%d" % (alias,len(entities)))
 	
 	print("Loading and annotating corpus...")
 	with open(args.inParsed,'rb') as f:
 		corpus = pickle.load(f)
 		
+	#corpus = kindred.Corpus(text = "Reports are coming from Wuhan in China")
+	#corpus.documents[0].metadata = {"title":"-"*100}
+	#parser = kindred.Parser(model='en_core_sci_sm')
+	#parser.parse(corpus)
+	
 	corpus.removeEntities()
 	ner = kindred.EntityRecognizer(termLookup)
 	ner.annotate(corpus)
+	
+	#doc = corpus.documents[0]
+	#print(doc.entities) 	
+	
+	#assert False
 	
 	print("Loading and integrating with JSON file...")
 	with open(args.inJSON) as f:
@@ -80,10 +78,42 @@ if __name__ == '__main__':
 	entity_map = {}
 	for d in corpus.documents:
 		title = d.metadata['title']
-		entities = []
+		
+		unambigLocationCoords = []
+		
+		entitiesByPosition = defaultdict(list)
 		for e in d.entities:
-			startPos = e.position[0][0]
-			endPos = e.position[0][1]
+			entitiesByPosition[e.position[0]].append(e)
+			
+		for position,entitiesAtPosition in entitiesByPosition.items():
+			if len(entitiesAtPosition) == 1 and entitiesAtPosition[0].entityType == 'location':
+				thisGeoData = geoData[entitiesAtPosition[0].externalID]
+				coord = [thisGeoData['longitude'],thisGeoData['latitude']]
+				unambigLocationCoords.append(coord)
+			
+		entities = []
+		for position,entitiesAtPosition in entitiesByPosition.items():
+			#allAreLocations = all( e.entityType=='location' for e in entitiesAtPosition )
+			
+			# Only do disambiguation for locations
+			#if len(entitiesAtPosition) > 1 and not allAreLocations and len(unambigLocationCoords) > 0:
+			#	continue
+			if len(entitiesAtPosition) > 1:
+				continue
+				
+			#if allAreLocations:
+			#	candidateCoords = []
+			#	for e in entitiesAtPosition:
+			#		thisGeoData = geoData[e.externalID]
+			#		coord = [thisGeoData['longitude'],thisGeoData['latitude']]
+			#		candidateCoords.append(coord)
+			#	closestCandidateCoord = distance_matrix(unambigLocationCoords, candidateCoords).min(axis=0).argmin()
+			#	entitiesAtPosition = [entitiesAtPosition[closestCandidateCoord]]
+				
+			e = entitiesAtPosition[0]
+			
+			startPos = position[0]
+			endPos = position[1]
 			if endPos <= len(title):
 				section = 'title'
 			else:
@@ -91,18 +121,19 @@ if __name__ == '__main__':
 				startPos -= len(title)+1
 				endPos -= len(title)+1
 				
-			if ';' in e.externalID:
-				continue
-				
 			normalized = allEntities[e.externalID]['name']
 			
 			entity = {''}
-			entity = {'start':startPos,'end':endPos,'section':section,'id':e.externalID,'type':e.entityType,'text':e.text,'normalized':normalized,}
+			entity = {'start':startPos,'end':endPos,'section':section,'id':e.externalID,'type':e.entityType,'text':e.text,'normalized':normalized}
 			entities.append(entity)
 			
-		key = tuple([ d.metadata[k] for k in key_items ])
 		#print(entities)
-		assert not key in entity_map
+			
+		#if d.metadata['cord_uid'] == 'zpv5f8pr':
+		#	print(json.dumps(entities,indent=2,sort_keys=True))
+			
+		key = tuple([ d.metadata[k] for k in key_items ])
+		assert not key in entity_map, "Found duplicate document with key: %s" % str(key)
 		entity_map[key] = entities
 		
 	for d in documents:
