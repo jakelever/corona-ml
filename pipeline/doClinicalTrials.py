9df4eac1d953f838e4d100b456704fb810de3ed6
Jake Lever <jake.lever@gmail.com>
2020-07-16 11:58:01 -0700

    Tweaks to the clinical trials script (using all the regexs)

--- pipeline/doClinicalTrials.py
+++ pipeline/doClinicalTrials.py
@@ -5,11 +5,10 @@ import re
 def predictTrial(abstractLower):
     clinicalTrialsGov = 'clinicaltrials.gov' in abstractLower
     trialNumber = re.search(r'ctr[0-9]+',abstractLower)
-    nihTrialNumber = re.search(r'nct[0-9]+',abstractLower)
+    nihTrialNumber = re.search(r'nct\s*[0-9]+',abstractLower)
     openLabel = 'open-label' in abstractLower or 'open label' in abstractLower
     
     decision = bool(clinicalTrialsGov or trialNumber or nihTrialNumber or openLabel)
-    decision = bool(trialNumber)
     return decision
 
 if __name__ == '__main__':
@@ -22,6 +21,7 @@ if __name__ == '__main__':
 	with open(args.inJSON) as f:
 		documents = json.load(f)
 		
+	trialCount = 0
 	for d in documents:
 		abstractLower = d['abstract'].lower()
 		predictedTrial = predictTrial(abstractLower)
@@ -31,9 +31,15 @@ if __name__ == '__main__':
 		
 		if not 'topics' in d:
 			d['topics'] = []
+		elif 'Clinical Trials' in d['topics']:
+			print("REMOVING")
+			d['topics'].remove('Clinical Trials')
 		
 		if annotatedTrial or meshTrial or predictedTrial:
 			d['topics'].append('Clinical Trials')
+			trialCount += 1
+			
+	print("Annotated %d documents as clinical trials" % trialCount)
 		
 	print("Saving JSON file...")
 	with open(args.outJSON,'w') as f:
