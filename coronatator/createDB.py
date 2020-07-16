407d670690f9c969cbe6ace6f506fd9f472fe4cb
Jake Lever <jake.lever@gmail.com>
2020-07-16 11:57:33 -0700

    Add an index to the annotationpositions table

--- coronatator/createDB.py
+++ coronatator/createDB.py
@@ -114,6 +114,7 @@ if __name__ == '__main__':
 
 	fields = ", ".join("%s %s" % (n,t) for n,t in columns.items())
 	fields += ", PRIMARY KEY(%s)" % list(columns.keys())[0]
+	fields += ", INDEX(annotation_id)"
 	sql = "CREATE TABLE annotationpositions (%s)" % fields
 	print(sql)
 	mycursor.execute(sql)
