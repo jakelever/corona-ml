a682b1f113d617f266bbc2d78493d85381e42e21
Jake Lever <jake.lever@gmail.com>
2020-08-04 12:53:16 -0700

    Added spotfixes

--- pipeline/integrateWebDataAndSpotfixes.py
+++ pipeline/integrateWebDataAndSpotfixes.py
@@ -42,17 +42,29 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser('Integrate in metadata from web scraping')
 	parser.add_argument('--inJSON',required=True,type=str,help='Input JSON documents')
 	parser.add_argument('--webmetadata',required=True,type=str,help='Scraped web data')
+	parser.add_argument('--spotfixes',required=True,type=str,help='List of mini fixes to corpus')
 	parser.add_argument('--outJSON',required=True,type=str,help='Output JSON with added metadata')
 	args = parser.parse_args()
-	
 	
 	print("Loading documents...")
 	with open(args.inJSON) as f:
 		documents = json.load(f)
 		
+	print("Applying spot fixes...")
+	with open(args.spotfixes) as f:
+		spotfixes = json.load(f)
+		
+	for spotfix in spotfixes:
+		field,fix_from,fix_to = spotfix['field'],spotfix['from'],spotfix['to']
+		search = [ d for d in documents if d[field] == fix_from ]
+		assert len(search) > 0, "Couldn't find documents for spotfix: %s" % str(spotfix)
+		for d in search:
+			d[field] = fix_to
+	
 	print("Loading webdata...")
 	with open(args.webmetadata) as f:
 		webmetadata = json.load(f)
+		
 		
 	journalFields = ['citation_journal_title', 'journal', 'journalName', 'journal_title', 'wkhealth_journal_title']
 		
