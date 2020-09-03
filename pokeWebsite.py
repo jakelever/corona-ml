007930c6f195c8309f645a07bac54d6bcbb2a09e
Jake Lever <jake.lever@gmail.com>
2020-09-03 16:04:54 -0700

    Attempt to fix poke website connection issues

--- pokeWebsite.py
+++ pokeWebsite.py
@@ -2,8 +2,10 @@ import argparse
 import requests
 import re
 import time
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+#from requests.adapters import HTTPAdapter
+#from requests.packages.urllib3.util.retry import Retry
+import urllib3
+import requests
 
 # From: https://www.peterbe.com/plog/best-practice-with-retries-with-requests
 def requests_retry_session(
@@ -25,6 +27,26 @@ def requests_retry_session(
 	session.mount('https://', adapter)
 	return session
 	
+def geturl(url,retries=10):
+	response = None
+	for retry in range(retries):
+		try:
+			response = requests.get(url)
+			break
+		except requests.exceptions.ConnectionError:
+			pass
+		except urllib3.exceptions.MaxRetryError:
+			pass
+		except urllib3.exceptions.NewConnectionError:
+			pass
+			
+		time.sleep((retry+1)*3)
+		
+	if response is None:
+		raise RuntimeError("Unable to connect to %s" % url)
+	else:
+		return response
+	
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Poke the main pages of CoronaCentral to force a relaxed rebuild")
 	parser.add_argument('--url',required=False,type=str,help='Alternative URL of CoronaCentral to poke (instead of coronacentral.ai)')
@@ -34,9 +56,12 @@ if __name__ == '__main__':
 	
 	print("Fetching list of pages to poke directly from CoronaCentral")
 	if args.url:
-		response = requests_retry_session(session=s).get(args.url.rstrip('/') + '/?notrack')
+		url = args.url.rstrip('/') + '/?notrack'
 	else:
-		response = requests_retry_session(session=s).get('https://www.coronacentral.ai/?notrack')
+		url = 'https://www.coronacentral.ai/?notrack'
+		
+	#response = requests_retry_session(session=s).get(url)
+	response = geturl(url)
 	
 	pages_to_poke = re.findall('"/[a-z]+"',response.text)
 	pages_to_poke = sorted(set( page.strip('"') for page in pages_to_poke))
@@ -48,7 +73,9 @@ if __name__ == '__main__':
 
 	for page in pages_to_poke:
 		print("Poking %s... " % page)
-		response = requests_retry_session(session=s).get('https://www.coronacentral.ai' + page + '?notrack' )
+		url = 'https://www.coronacentral.ai' + page + '?notrack'
+		#response = requests_retry_session(session=s).get(url)
+		response = geturl(url)
 		assert response.status_code == 200
 		time.sleep(5)
 	
