a8b8ebe4d89ce47499f99f6cadfa18e8dcbd960d
Jake Lever <jake.lever@gmail.com>
2021-04-06 16:06:49 -0700

    Save tweet time after success

--- twitter/dotweet.sh
+++ twitter/dotweet.sh
@@ -7,9 +7,10 @@ if [[ "$last_tweet" == "$today" ]]; then
 	echo "Already tweeted today. No more!"
 	exit 0
 fi
-echo $today > last_tweet.log
 
 latestAltmetric=`ls -tr ../altmetric/recent_altmetric.json ../pipeline/data/altmetric.json | tail -n 1`
 
 python tweet.py --documents ../pipeline/data/coronacentral.json --altmetric $latestAltmetric --twitterApiKey twitterApiKey.json
+
+echo $today > last_tweet.log
 
