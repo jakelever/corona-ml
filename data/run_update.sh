532c4c821ca150ac3398d6b2d4364b5af8b560b9
Jake Lever <jake.lever@gmail.com>
2023-07-10 10:19:09 +0100

    No need to check Cord19

--- data/run_update.sh
+++ data/run_update.sh
@@ -5,7 +5,5 @@ bash updatePubmedListing.sh
 
 snakemake --cores 1 -T 3
 
-bash downloadCORD19_metadataOnly.sh 
-
 python collectAllPapers.py --cord19Metadata cord19/metadata.csv --pubmed pubmed_corona/ --outFile ../pipeline/data/alldocuments.json
 
