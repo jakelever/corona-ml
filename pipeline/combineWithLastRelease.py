ffa34e646c014ab00c8b6313de173b3b6d225c19
Jake Lever <jake.lever@gmail.com>
2023-07-12 11:22:06 +0100

    Add scripts to combine with previous release and record previous document ID info

---
+++ pipeline/combineWithLastRelease.py
@@ -0,0 +1,37 @@
+import argparse
+import json
+import gzip
+
+def main():
+	parser = argparse.ArgumentParser('Combine the output with the last release of CoronaCentral')
+	parser.add_argument('--inJSON',required=True,type=str,help='Input JSON documents')
+	parser.add_argument('--lastRelease',required=True,type=str,help='coronacentral.json.gz file from last release')
+	parser.add_argument('--outJSON',required=True,type=str,help='Output JSON')
+	args = parser.parse_args()
+		
+	print("Loading last release documents...")
+	added = 0
+	with gzip.open(args.lastRelease,'rt') as f:
+		documents = json.load(f)
+		pubmed_ids = set( d['pubmed_id'] for d in documents )
+		print(f"Loaded {len(documents)} documents from last release")
+
+	print("Loading current run...")
+	with open(args.inJSON) as f:
+		current_run = json.load(f)
+		print(f"Loaded {len(current_run)} documents from current run")
+		for d in current_run:
+			if d['pubmed_id'] not in pubmed_ids:
+				pubmed_ids.add(d['pubmed_id'])
+				documents.append(d)
+				added += 1
+	print(f"Added {added} documents from current run to last release")
+			
+	print("Saving data...")
+	with open(args.outJSON,'w',encoding='utf8') as f:
+		json.dump(documents,f)
+	print(f"Saved {len(documents)} documents")
+
+if __name__ == '__main__':
+	main()
+
---
+++ pipeline/recordProcessedFiles.py
@@ -0,0 +1,34 @@
+import argparse
+import json
+import os
+import gzip
+
+def main():
+	parser = argparse.ArgumentParser('Note which files and Pubmed IDs have been processed')
+	parser.add_argument('--pubmedDir',required=True,type=str,help='Directory with PubMed files')
+	parser.add_argument('--initialDocuments',required=True,type=str,help='Initial documents file')
+	parser.add_argument('--finalRelease',required=True,type=str,help='Final file')
+	parser.add_argument('--outJSONGZ',required=True,type=str,help='Output JSON GZ')
+	args = parser.parse_args()
+	
+	pubmed_files = sorted( [ f for f in os.listdir(args.pubmedDir) if f.startswith('pubmed') ] )
+	
+	print("Loading documents...")
+	with open(args.initialDocuments) as f:
+		documents = json.load(f)
+		pubmed_ids = [ d['pubmed_id'] for d in documents if d['pubmed_id'] ]
+	
+	with open(args.finalRelease) as f:
+		documents = json.load(f)
+		pubmed_ids += [ d['pubmed_id'] for d in documents if d['pubmed_id'] ]
+		
+	pubmed_ids = sorted(set(pubmed_ids))
+		
+	print("Saving data...")
+	with gzip.open(args.outJSONGZ,'wt') as f:
+		output = {'pubmed_ids':pubmed_ids, 'pubmed_files':pubmed_files}
+		json.dump(output,f)
+
+if __name__ == '__main__':
+	main()
+
