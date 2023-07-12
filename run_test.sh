b0fecd1b6976f6d684cac7296369542fd74de675
Jake Lever <jake.lever@gmail.com>
2023-07-12 13:26:40 +0100

    Update test run with empty old CoronaCentral release

--- run_test.sh
+++ run_test.sh
@@ -14,7 +14,13 @@ cp pipeline/*.json testing_dir
 cp -r pipeline/predefined testing_dir
 
 # Copy the small test dataset into the data directory
-cp test_documents.json testing_dir/data/alldocuments.json
+gzip -c test_documents.json > testing_dir/data/alldocuments.json.gz
+
+# Set up a dummy empty old release of CoronaCentral
+rm -fr last_release
+mkdir last_release
+echo '[]' | gzip > last_release/coronacentral.json.gz
+echo '{"pubmed_ids":[], "pubmed_files":[]}' | gzip > last_release/process_record.json.gz
 
 # Move into the directory
 cd testing_dir
